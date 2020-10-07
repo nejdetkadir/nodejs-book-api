16b820dd96c4de414297824e663b39d4b5d8a3f8
nejdetkadir <nejdetkadir.550@gmail.com>
2020-10-07 03:40:36 +0300

    Create get route of single  author and theirs books

--- routes/authors.js
+++ routes/authors.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 const Author = require('../models/Author');
@@ -16,6 +17,57 @@ router.post('/', (req, res) => {
 /* GET authors and theirs books. */
 router.get('/', (req, res) => {
   const promise = Author.aggregate([
+    {
+      $lookup: {
+        from: 'books',
+        localField: '_id',
+        foreignField: 'author_id',
+        as: 'books'
+      }
+    },
+    {
+      $unwind: {
+        path: '$books',
+        preserveNullAndEmptyArrays: true
+      }
+    },
+    {
+      $group: {
+        _id: {
+          _id: '$_id',
+          name: '$name',
+          surname: '$surname',
+          bio: '$bio'
+        },
+        books: {
+          $push: '$books'
+        }
+      }
+    },
+    {
+      $project: {
+        _id: '$_id._id',
+        name: '$_id.name',
+        surname: '$_id.surname',
+        books: '$books'
+      }
+    }
+  ]);
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  })
+});
+
+/* GET single author and theirs books. */
+router.get('/:author_id', (req, res) => {
+  const promise = Author.aggregate([
+    {
+      $match: {
+        '_id': mongoose.Types.ObjectId(req.params.author_id)
+      }
+    },
     {
       $lookup: {
         from: 'books',
