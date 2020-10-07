e7142ecb0e84c6ed66c98244d820b5cd7799d308
nejdetkadir <nejdetkadir.550@gmail.com>
2020-10-07 18:25:01 +0300

    Add test scripts

---
+++ test/index.test.js
@@ -0,0 +1,14 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+const server = require('../app');
+
+chai.use(chaiHttp);
+describe('Node Server', () => {
+    it('(GET /) index page', (done) => {
+        chai.request(server).get('/').end((err, res) => {
+           res.should.have.status(200);
+           done();
+        });
+    });
+});
