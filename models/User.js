2274bdd2d6afbad2acdc0295bbad9e98371b5410
nejdetkadir <nejdetkadir.550@gmail.com>
2020-10-07 17:03:15 +0300

    Create user model and register route

---
+++ models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: [2, '`{PATH}` is very short for inserting. (min: {MINLENGTH})'],
+        maxlength: [40, '`{PATH}` is very long for inserting. (max: {MAXLENGTH})']
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: [2, '`{PATH}` is very short for inserting. (min: {MINLENGTH})'],
+        maxlength: [20, '`{PATH}` is very long for inserting. (max: {MAXLENGTH})']
+    }
+});
+
+module.exports = mongoose.model('user', UserSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+/* POST user register. */
+router.post('/register', (req, res, next) => {
+  const {username, password} = req.body;
+  const user = new User({
+    username,
+    password
+  });
+  const promise = user.save();
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
 });
 
 module.exports = router;
